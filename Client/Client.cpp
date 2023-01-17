963907518992fe04a9e920c9b3e048405d229502
Gavin Abeele <gabeele2160@conestogac.on.ca>
2023-01-17 12:48:19 -0500

    Added comments for what the program is doing.

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -18,55 +18,55 @@ int main()
 	char Rx[128];
 
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
-	ClientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	ClientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);	// IPv4 TCP protocol 
 	SvrAddr.sin_family = AF_INET;
-	SvrAddr.sin_port = htons(27001);
-	SvrAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	SvrAddr.sin_port = htons(27001);	// Port 27001
+	SvrAddr.sin_addr.s_addr = inet_addr("127.0.0.1");	//Localhost
 	connect(ClientSocket, (struct sockaddr*)&SvrAddr, sizeof(SvrAddr));
 
-	uiSize = GetSize();
-	for (unsigned int l = 0; l < uiSize; l++)
+	uiSize = GetSize();	// Obtains the number of lines in the file
+	for (unsigned int l = 0; l < uiSize; l++)	// For each line in the file. l = line NOT 1!
 	{
 		string strInput;
 		ifstream ifs("DataFile.txt");
-		for (unsigned int iStart = 0; iStart < l; iStart++)
+		for (unsigned int iStart = 0; iStart < l; iStart++)	// Finds the starting line by running through the file after the previously read line
 			getline(ifs, strInput);
 
-		getline(ifs, strInput);
-		if (l > 0)
+		getline(ifs, strInput);	// Gets the current line
+		if (l > 0)	// Only after the first iteration will this execute
 		{
 			size_t offset, preOffset;
 			offset = preOffset = 0;
 			unsigned int iParamIndex = 0;
 			//while (offset != std::string::npos)
-			while(iParamIndex != 8)
+			while(iParamIndex != 8)	// Sets 8 iterations of the data file 
 			{
-				offset = strInput.find_first_of(',', preOffset+1);
+				offset = strInput.find_first_of(',', preOffset+1);	// Finds the first comma and sets the offset to the index of the comma + 1 
 				string strTx = strInput.substr(preOffset+1, offset - (preOffset+1));
-				send(ClientSocket, ParamNames[iParamIndex].c_str(), (int)ParamNames[iParamIndex].length(), 0);
-				recv(ClientSocket, Rx, sizeof(Rx), 0);
-				send(ClientSocket, strTx.c_str(), (int)strTx.length(), 0);
-				recv(ClientSocket, Rx, sizeof(Rx), 0);
-				cout << ParamNames[iParamIndex] << " Avg: " << Rx << endl;
-				preOffset = offset;
-				iParamIndex++;
+				send(ClientSocket, ParamNames[iParamIndex].c_str(), (int)ParamNames[iParamIndex].length(), 0);		// Sends the current parameter title (What the data is)
+				recv(ClientSocket, Rx, sizeof(Rx), 0);		// Waits for response
+				send(ClientSocket, strTx.c_str(), (int)strTx.length(), 0);		// Sends the data 
+				recv(ClientSocket, Rx, sizeof(Rx), 0);		// Waits for response
+				cout << ParamNames[iParamIndex] << " Avg: " << Rx << endl;		// Prints what was recevied 
+				preOffset = offset;	// Sets the pre offset to the next offset to get the next piece of data
+				iParamIndex++;	// Increment the param index
 			}
 		}
-		else
+		else // Runs the first line to get the column names
 		{
-			ParamNames.push_back("TIME STAMP");
-			size_t offset, preOffset;
+			ParamNames.push_back("TIME STAMP");		// Pushes time stamp into the param names vector
+			size_t offset, preOffset;	
 			offset = 0;
 			preOffset = -1;
-			while (offset != std::string::npos)
+			while (offset != std::string::npos)		// While it is not the end of the string
 			{
-				offset = strInput.find_first_of(',', preOffset + 1);
-				string newParam = strInput.substr(preOffset + 1, offset - (preOffset + 1));
-				ParamNames.push_back(newParam);
+				offset = strInput.find_first_of(',', preOffset + 1);	// Parse by comma
+				string newParam = strInput.substr(preOffset + 1, offset - (preOffset + 1));	// Get the parameter 
+				ParamNames.push_back(newParam);	// Pushes the new parameter to the param names
 				preOffset = offset;
 			}
 		}
-		ifs.close();
+		ifs.close();	 // Closes the file
 	}
 
 	closesocket(ClientSocket);
@@ -75,11 +75,12 @@ int main()
 	return 1;
 }
 
+// Obtains the number of lines in the file
 unsigned int GetSize()
 {
 	string strInput;
 	unsigned int uiSize = 0;
-	ifstream ifs("DataFile.txt");
+	ifstream ifs("DataFile.txt");	// Specifically in this file
 	if (ifs.is_open())
 	{
 		while (!ifs.eof())
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 struct StorageTypes 
 { 
-	unsigned int size = 0;
-	float* pData;
+	unsigned int size = 0;	// Length of the column 
+	float* pData;			// The actual value
 };
-StorageTypes RxData[7];
+StorageTypes RxData[7];		// Creates 8 RxData units
 
 void UpdateData(unsigned int, float);
 float CalcAvg(unsigned int);
@@ -21,7 +21,7 @@ int main()
 	sockaddr_in SvrAddr;
 
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
-	ServerSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	ServerSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);	
 	if (ServerSocket == SOCKET_ERROR)
 		return -1;
 
@@ -43,19 +43,20 @@ int main()
 
 	cout << "Connection Established" << endl;
 
-	while (RxBuffer[0] != '*')
+	while (RxBuffer[0] != '*')	// The 
 	{
 		float fValue = 0;
-		memset(RxBuffer, 0, sizeof(RxBuffer));
-		recv(ConnectionSocket, RxBuffer, sizeof(RxBuffer), 0);
-		send(ConnectionSocket, "ACK", sizeof("ACK"), 0);
+		memset(RxBuffer, 0, sizeof(RxBuffer));	// Making the RxBuffer 0
+		recv(ConnectionSocket, RxBuffer, sizeof(RxBuffer), 0);		// Reveices the buffer
+		send(ConnectionSocket, "ACK", sizeof("ACK"), 0);	// Sends an ackknowledgment
+		// Checks what the buffer's string has
 		if (strcmp(RxBuffer, "ACCELERATION BODY X") == 0)
 		{
-			memset(RxBuffer, 0, sizeof(RxBuffer));
-			size_t result = recv(ConnectionSocket, RxBuffer, sizeof(RxBuffer), 0);
-			fValue = (float)atof(RxBuffer);
-			UpdateData(0, fValue);
-			fValue = CalcAvg(0);
+			memset(RxBuffer, 0, sizeof(RxBuffer)); // Clearing RxBuffer
+			size_t result = recv(ConnectionSocket, RxBuffer, sizeof(RxBuffer), 0);		// Receives the next piece of data
+			fValue = (float)atof(RxBuffer);	// Assigns the float value from the buffer
+			UpdateData(0, fValue);	// Calls to update
+			fValue = CalcAvg(0);		// Calculates the average
 		}
 		else if (strcmp(RxBuffer, "ACCELERATION BODY Y") == 0)
 		{
@@ -107,11 +108,13 @@ int main()
 		}
 		else
 		{
+			// Missing time stamp -- will lead to the time stampe being printed at 0.0000 
 			memset(RxBuffer, 0, sizeof(RxBuffer));
 			recv(ConnectionSocket, RxBuffer, sizeof(RxBuffer), 0);
 			fValue = 0.0;
 		}
 
+		// Sends the average back to the client
 		char Tx[128];
 		sprintf_s(Tx, "%f", fValue);
 		send(ConnectionSocket, Tx, sizeof(Tx), 0);
@@ -124,31 +127,34 @@ int main()
 	return 1;
 }
 
+// 
 void UpdateData(unsigned int uiIndex, float value)
 {
+	// First time a value is entered will configure the column
 	if (RxData[uiIndex].size == 0)
 	{
-		RxData[uiIndex].pData = new float[1];
+		RxData[uiIndex].pData = new float[1];	
 		RxData[uiIndex].pData[0] = value;
 		RxData[uiIndex].size = 1;
 	}
-	else
+	else     // Any time there is more than 1 in the column, it will reconfigure the memory 
 	{
-		float* pNewData = new float[RxData[uiIndex].size + 1];
-		for (unsigned int x = 0; x < RxData[uiIndex].size; x++)
+		float* pNewData = new float[RxData[uiIndex].size + 1]; // re-creating the float pointer with memory space for 1 additional float
+		for (unsigned int x = 0; x < RxData[uiIndex].size; x++)	// Iterate through the old values and assign the old values into the newly created float ptr array.
 			pNewData[x] = RxData[uiIndex].pData[x];
 
-		pNewData[RxData[uiIndex].size] = value;
-		delete[] RxData[uiIndex].pData;
-		RxData[uiIndex].pData = pNewData;
-		RxData[uiIndex].size++;
+		pNewData[RxData[uiIndex].size] = value; // appends the newly received float onto the end of the array
+		delete[] RxData[uiIndex].pData; // free old memory
+		RxData[uiIndex].pData = pNewData; // reassign the structure member variable
+		RxData[uiIndex].size++; // incrememnt the structure member size counter
 	}
 }
 
+// Calcuates the average in all the values from 0 to uiIndex. It is a running average
 float CalcAvg(unsigned int uiIndex)
 {
 	float Avg = 0;
-	for (unsigned int x = 0; x < RxData[uiIndex].size; x++)
+	for (unsigned int x = 0; x < RxData[uiIndex].size; x++)	// Calculates the average of the current column lenght 
 		Avg += RxData[uiIndex].pData[x];
 
 	Avg = Avg / RxData[uiIndex].size;

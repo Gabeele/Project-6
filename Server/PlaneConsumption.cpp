c2b177ad8572f17f660e9b6ff72ece8109336944
karl <karldstencell@gmail.com>
2023-02-27 14:23:12 -0500

    added plane consumption class

---
+++ Server/PlaneConsumption.cpp
@@ -0,0 +1,10 @@
+#include "PlaneConsumption.h"
+
+PlaneConsumption::PlaneConsumption(string PlaneId) {
+	this->PlaneId = PlaneId;
+}
+
+float PlaneConsumption::calcAverage(time_t date, float data)
+{
+	return 0.0f;
+}
--- Server/PlaneConsumption.h
+++ Server/PlaneConsumption.h
@@ -1,1 +1,18 @@
 #pragma once
+
+
+#include <string>
+using namespace std;
+
+class PlaneConsumption
+{
+
+public:
+	string PlaneId;
+	float Average = 0;
+	int DataPoints = 0;
+
+	explicit PlaneConsumption(string PlaneId);
+	float calcAverage(time_t date, float data);
+
+};
